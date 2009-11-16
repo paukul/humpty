59465d2a51f2e125f8a6ca3273ff450534f97191
Pascal Friederich <paukul@gmail.com>
2009-11-16 23:58:17 +0100

    some more fugly css

--- amqp_monitor.rb
+++ amqp_monitor.rb
@@ -7,13 +7,20 @@ class Server
   base_uri "http://localhost:9999"
   
   def self.queues
-    requested_attributes = ["name", "durable", "auto_delete", "arguments", "messages_ready", "messages_unacknowledged", "messages_uncommitted", "messages", "acks_uncommitted", "consumers", "transactions", "memory"]
-    puts "/queues/root/#{requested_attributes.join('/')}"
+    requested_attributes = %w(name durable auto_delete arguments messages_ready messages_unacknowledged messages_uncommitted messages acks_uncommitted consumers transactions memory)
     get("/queues/root/#{requested_attributes.join('/')}")["queues"]
+  end
+  
+  def self.bindings
+    
   end
 end
 
 get '/' do
+  redirect "/queues"
+end
+
+get '/queues/?' do
   @queues = Server.queues
   haml :queues
 end
